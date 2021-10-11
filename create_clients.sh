ba3923e0aacbc19ae312dafb2c06b8183d6026d7
vpnalx <vpn525@gmail.com>
2021-10-11 13:41:56 +0000

    all

--- create_clients.sh
+++ create_clients.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#Creating directories for client keys and configs
+#checking if the script is being run as sudo or by root useir
+if (($EUID != 0)); then
+	echo "Please run this script as root or using sudo"
+        exit
+fi
 
-sudo mkdir -p foo /etc/wireguard/client_keys
-sudo mkdir -p foo /etc/wireguard/configs
+#Creating directories for client keys and configs
+sudo mkdir -p /etc/wireguard/client_keys
+sudo mkdir -p /etc/wireguard/configs
 
 echo " enter the name of the client"
 read client_name
@@ -17,6 +22,7 @@ umask 077
 
 sudo wg genkey | tee "$client_name".private.key | wg pubkey > "$client_name".public.key
 
+cd
 
 #Using the number of config files to automatically increment the Last Octect(LO) of ip address for each client configuration file.
 
@@ -25,7 +31,7 @@ LOV=$((LO+10))
 # LOV= last octect value, ie, adding 10 to the number of configs to use values from 10.5.0.10,11,12,13,14....etc
 
 echo "[Interface]
-PrivateKey = $(cat "/etc/wireguard/clients/"$client_name".private.key")
+PrivateKey = $(cat /etc/wireguard/client_keys/"$client_name".private.key)
 ListenPort = 51820
 Address = 10.5.0."$LOV"/24 " > /etc/wireguard/configs/$client_name.conf
 
@@ -44,9 +50,11 @@ AllowedIPs =10.5.0."$LOV"/32" >> /etc/wireguard/wg0.conf
 
 echo "udpated server configuration with the new client information"
 
-echo "deleting key files from /etc/wireguard/clent_keys"
+echo "deleting key files from /etc/wireguard/client_keys"
 sudo rm -rf /etc/wireguard/client_keys/
 
 echo "done"
 
 sudo systemctl restart wg-quick@wg0.service
+
+
