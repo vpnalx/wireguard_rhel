528d8354e03ff5e865e44aa1a2d69642d29a9b65
vpnalx <vpn525@gmail.com>
2021-10-13 08:48:21 +0000

    a

--- helpwg.sh
+++ helpwg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $'*************************************\nUse these commands to configure your wireguard clients'
-echo $'\nvpnA  :- Add a new client\nvpnD  :- Delete a client\nvpnDA :- Delete all clients\nvpnU  :- Uninstall wireguard VPN and all configuration'
+echo $'\nvpn   :- Display help message\nvpnA  :- Add a new client\nvpnQ  :- Generate QR code for client configuration\nvpnD  :- Delete a client\nvpnDA :- Delete all clients\nvpnU  :- Uninstall wireguard VPN and all configuration'
 echo $'\n*************************************'
