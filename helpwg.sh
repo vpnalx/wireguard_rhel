1e7944531efc547614cac3c112317612e01beda4
vpnalx <vpn525@gmail.com>
2021-10-13 07:18:52 +0000

    new

---
+++ helpwg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo $'*************************************\nUse these commands to configure your wireguard clients'
+echo $'\nvpnA  :- Add a new client\nvpnD  :- Delete a client\nvpnDA :- Delete all clients\nvpnU  :- Uninstall wireguard VPN and all configuration'
+echo $'\n*************************************'
---
+++ install_server.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+#checking if the script is being run as sudo or by root useir
+
+if (($EUID != 0)); then
+        echo "Please run this script as root or using sudo"
+        exit
+fi
+
+apt update -y
+apt upgrade -y
+apt install wireguard -y
+apt install wireguard-tools -y
+
+#Uncommenting the ipv4 forward line from /etc/sysctl.conf
+
+sed -i '/net.ipv4.ip_forward=1 /s/^#//g' /etc/sysctl.conf
+
+sysctl -p
+
+echo " creating server configuration"
+
+cd /etc/wireguard
+umask 077
+wg genkey | tee private.key | wg pubkey > public.key
+
+echo "
+[Interface]
+PrivateKey = $(sudo cat /etc/wireguard/private.key)
+Address = 10.5.0.1/24
+MTU = 1420
+ListenPort = 51820" > wg0.conf
+
+wg-quick up wg0
+systemctl enable wg-quick@wg0
+
+echo " Downloading other required files for client configurarion.."
+
+
+cd 
+wget https://raw.githubusercontent.com/vpnalx/wireguard/main/create_clients.sh
+wget https://raw.githubusercontent.com/vpnalx/wireguard/main/remove_all_clients.sh
+wget https://raw.githubusercontent.com/vpnalx/wireguard/main/uninstall_wg.sh
+
+mv create_clients.sh /usr/local/bin/vpnA
+mv remove_all_clients.sh /usr/local/bin/vpnDA
+mv uninstall_wg.sh /usr/local/bin/vpnU
+
+cd /usr/local/bin
+
+chmod +x vpnA vpnDA vpnU
+
+
+echo -n "It is recommended to reboot the system. Enter 'yes' to reboot :   "
+
+read input
+
+if [ "$input" = "yes" ] ;then
+        reboot
+else
+        echo " Manually reboot the system later"
+fi
