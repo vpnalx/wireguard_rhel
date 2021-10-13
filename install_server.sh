0bbaca3a9423ca13ecced1d9ea3e19d31b28f02e
vpnalx <57708153+vpnalx@users.noreply.github.com>
2021-10-13 13:57:34 +0400

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -60,7 +60,7 @@ chmod +x vpnA vpnDA vpnU vpnD vpnQ vpn
 
 echo -e "It is recommended to reboot the system. \nEnter 'yes' to reboot :   "
 
-read input
+read -p input
 
 if [ "$input" = "yes" ] ;then
         reboot
