82cc1339754eda8812498749eeffecdfc2a0f79a
vpnalx <57708153+vpnalx@users.noreply.github.com>
2021-10-16 17:52:51 +0400

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -30,7 +30,9 @@ echo "
 PrivateKey = $(sudo cat /etc/wireguard/private.key)
 Address = 10.5.0.1/24
 MTU = 1420
-ListenPort = 51820" > wg0.conf
+ListenPort = 51820
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE " > wg0.conf
 
 wg-quick up wg0
 systemctl enable wg-quick@wg0
@@ -38,7 +40,7 @@ systemctl enable wg-quick@wg0
 echo " Downloading other required files for client configurarion.."
 
 
-cd 
+cd
 wget https://raw.githubusercontent.com/vpnalx/wireguard/main/create_clients.sh
 wget https://raw.githubusercontent.com/vpnalx/wireguard/main/remove_all_clients.sh
 wget https://raw.githubusercontent.com/vpnalx/wireguard/main/uninstall_wg.sh
@@ -58,6 +60,10 @@ cd /usr/local/bin
 
 chmod +x vpnA vpnDA vpnU vpnD vpnQ vpn vpnCT
 
+echo echo "enter the public IP or DNS name of your server"
+read PublicIP
+echo $PublicIP > /etc/wireguard/PublicIP
+
 echo $'*************************************\nUse these commands to configure your wireguard clients'
 echo $'\nvpn   :- Display help message\nvpnA  :- Add a new client\nvpnCT  :- Show client configuration in Text\nvpnQ  :- Generate QR code for client configuration\nvpnD  :- Delete a client\nvpnDA :- Delete all clients\nvpnU  :- Uninstall wireguard VPN and all configuration'
 echo $'\n*************************************'
@@ -69,4 +75,3 @@ if [ "$input" = "yes" ] ;then
         reboot
 else
         echo " Manually reboot the system later"
-fi
