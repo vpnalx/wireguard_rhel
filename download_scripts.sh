9cb13e54565314c079fa1b975cd2f822ae4e1004
vpnalx <vpn525@gmail.com>
2021-10-16 15:26:41 +0000

    download_scripts.sh

--- download_scripts.sh
+++ download_scripts.sh
@@ -4,13 +4,17 @@ if (($EUID != 0)); then
         echo "Please run this script as root or using sudo"
         exit
 fi
-echo "donwloading wireguard server installation script from https://raw.githubusercontent.com/vpnalx/wireguard/wg_server_install_script"
+echo "Downloading wireguard server installation script from https://raw.githubusercontent.com/vpnalx/wireguard/wg_server_install_script"
 
 sudo wget https://raw.githubusercontent.com/vpnalx/wireguard/main/install_server.sh
 
+echo " making the script executable"
+
 sudo chmod +x install_server.sh
 
-sudo ./install_server.sh
+
+
+echo 'Run the script using "sudo ./install_server"  to install wireguard server and download client configuration scripts to this machine"
 
 
 
